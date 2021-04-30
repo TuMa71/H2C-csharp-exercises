b6123b80a329c983e6adea077af74bf8337622e6
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 15:41:39 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_28
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("How old are you? ");
+      int age = Convert.ToInt32(Console.ReadLine());
+      if (age < 18)
+            {
+                Console.WriteLine("You're under age!");
+            } else
+            {
+                Console.WriteLine("Ypu're an adult!");
+            }
+      Console.ReadLine();
     }
   }
 }
