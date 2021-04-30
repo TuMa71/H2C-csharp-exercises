959cb1b407c7f57d1ed42b8152df6ab994df3d2f
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 10:49:25 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -8,6 +8,22 @@ namespace exercise_15
     {
 
       // Write your code here:
+      Console.WriteLine("Give a string: ");
+      string inputString = Console.ReadLine();
+      Console.WriteLine("Give an integer: ");
+      string userInt = Console.ReadLine();
+      int intValue = Convert.ToInt32(userInt);
+      Console.WriteLine("Give a double: ");
+      string userDouble = Console.ReadLine();
+      double doubleValue = Convert.ToDouble(userDouble);
+      Console.WriteLine("Give a boolean: ");
+      string userBoolean  = Console.ReadLine();
+      Boolean  booleanValue = Convert.ToBoolean(userBoolean);
+      Console.Write("Your string: " + inputString + "\n");
+      Console.Write("Your integer: " + intValue + "\n");
+      Console.Write("Your double: " + doubleValue + "\n");
+      Console.Write("Your boolean: " + booleanValue + "\n");
+      
 
     }
   }
