fb81bbf1df24e91b9f0ff9306550a8dad5695510
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 14:37:26 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -8,6 +8,16 @@ namespace exercise_21
     {
 
       // Write your code here:
+      // Write your code here:
+      Console.WriteLine("Give the first number! ");
+      string first = Console.ReadLine();
+      Console.WriteLine("Give the second number! ");
+      string second = Console.ReadLine();
+      int intFirst = Convert.ToInt32(first);
+      int intSecond = Convert.ToInt32(second);
+      double ave = (intFirst + intSecond) / 2.0;
+      Console.WriteLine("The average is " + ave);
+      Console.ReadLine();
 
     }
   }
