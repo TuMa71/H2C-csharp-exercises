975c944b974834fce85ace2baa798a303fe78de9
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 11:07:20 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -8,6 +8,12 @@ namespace exercise_16
     {
 
       // Write your code here:
+      Console.WriteLine("How many days? ");
+      string userInput = Console.ReadLine();
+      int numberOfDays = Convert.ToInt32(userInput);
+      int seconds = numberOfDays * 24 * 60 * 60;
+      Console.WriteLine(seconds);
+      Console.ReadLine();
 
     }
   }
