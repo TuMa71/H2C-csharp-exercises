08e541c2d7eb5b85ad02c348b25b76d3f33ac60b
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 11:50:28 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -8,6 +8,18 @@ namespace exercise_18
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number! ");
+      string first = Console.ReadLine();
+      Console.WriteLine("Give the second number! ");
+      string second = Console.ReadLine();
+      Console.WriteLine("Give the third number! ");
+      string third = Console.ReadLine();
+      int intFirst = Convert.ToInt32(first);
+      int intSecond = Convert.ToInt32(second);
+      int intThird = Convert.ToInt32(third);
+      int sum = intFirst + intSecond + intThird;
+      Console.WriteLine("The sum is " + sum);
+      Console.ReadLine();
 
     }
   }
