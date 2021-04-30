0330913493b312f5c5a737220ad1830a9f8d6220
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 14:58:50 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_23
     {
 
       // Write your code here:
+       Console.WriteLine("Give the first number! ");
+      string first = Console.ReadLine();
+      Console.WriteLine("Give the second number! ");
+      string second = Console.ReadLine();
+      int intFirst = Convert.ToInt32(first);
+      int intSecond = Convert.ToInt32(second);
+      int sum = intFirst + intSecond;
+      int min = intFirst - intSecond;
+      int mul = intFirst * intSecond;
+      double div = intFirst / (double)intSecond;
+      Console.WriteLine(intFirst + " + " + intSecond + " = " + sum);
+      Console.WriteLine(intFirst + " - " + intSecond + " = " + min);
+      Console.WriteLine(intFirst + " * " + intSecond + " = " + mul);
+      Console.WriteLine(intFirst + " / " + intSecond + " = " + div);
+      Console.ReadLine();
 
     }
   }
