25870a255a1b3b6c428d0760f9e6e4a83675e5c3
TuMa71 <tuomo.malinen@centria.fi>
2021-04-30 18:25:35 +0300

    30.4.2021

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Speak, friend and enter! ");
+      string speak = Console.ReadLine();
+      if (speak == "Mellon")
+            {
+                Console.WriteLine("Welcome, friend");
 
+            } else
+            {
+                Console.WriteLine("They've got a cave troll! ");
+            }
+      Console.ReadLine();
     }
   }
 }
