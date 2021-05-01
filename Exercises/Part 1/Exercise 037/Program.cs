4bd3a5753189febfb177c175bea34f64466bef92
TuMa71 <tuomo.malinen@centria.fi>
2021-05-01 17:17:07 +0300

    1.5.2021

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
-
+      while (true)
+            {
+                Console.WriteLine("Give a number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == 0)
+                {
+                    break;
+                } else
+                {
+                    sum = sum + 1;
+                }
+       
+    }
+    Console.WriteLine("Total amooun of numbers: " + sum);
+    Console.ReadLine();
     }
   }
 }
