1c48fa5bca32749bcaaa65d06fc81e15f9f6c14c
TuMa71 <tuomo.malinen@centria.fi>
2021-05-01 17:07:01 +0300

    1.5.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+       while (true)
+            {
+                Console.WriteLine("Give a number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == 0)
+                {
+                    break;
+                } else if (userInput < 0 )
+                {
+                    Console.WriteLine("That is negative");
+                } else
+                {
+                    Console.WriteLine(userInput * userInput);
+                }
+
+            }
 
     }
   }
