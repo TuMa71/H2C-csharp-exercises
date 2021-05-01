80a7089060a6287b89d1594299beb63e9c5bc86e
TuMa71 <tuomo.malinen@centria.fi>
2021-05-01 17:25:09 +0300

    1.5.2021

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+     int sum = 0;
+     while (true)
+            {
+                Console.WriteLine("Give a number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == 0)
+                {
+                    break;
+                } else if (userInput < 0)
+                {
+                    sum = sum + 1;
+                }
+       
+    }
+    Console.WriteLine("Total amoount of  negative numbers: " + sum);
+    Console.ReadLine(); 
 
     }
   }
