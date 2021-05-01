2b55483c9eec06ac861942aae5381480fc47ebe6
TuMa71 <tuomo.malinen@centria.fi>
2021-05-01 17:32:02 +0300

    1.5.2021

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -7,6 +7,23 @@ namespace exercise_39
     public static void Main(string[] args)
     {
       // Write your code here:
+      int sum = 0;
+      while (true)
+            {
+                Console.WriteLine("Give a number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == 0)
+                {
+                    break;
+                } else 
+                {
+                    sum = sum + userInput;
+                }
+       
+    }
+    Console.WriteLine("Total sum of numbers: " + sum);
+    Console.ReadLine(); 
+
     }
   }
 }
